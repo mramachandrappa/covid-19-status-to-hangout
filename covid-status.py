4cdca15e1777e51e3e68bd3bdf4a2f6adba89294
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-04-04 19:41:02 +0530

    Updated script

--- covid-status.py
+++ covid-status.py
@@ -1,33 +1,17 @@
 import urllib.request
 from bs4 import BeautifulSoup
-from prettytable import PrettyTable
 from httplib2 import Http
 from json import dumps
-from PIL import Image, ImageDraw, ImageFont
-import pandas as pd
 import datetime
 import re
+import sys
 
 
-class CovidIndia():
+class Covid19_Report_IN():
 
-    def hangout(self, active, ind_national, foreign_national, cured, deaths, total):
+    def smartshift_Bot(self, message, webhook_url):
 
-        url = '<INCOMING-WEBHOOK-URL>'
-        currentDT = datetime.datetime.now()
-
-        date = currentDT.strftime("%a, %b %d, %Y") + ' | ' + currentDT.strftime("%I:%M:%S %p")
-
-        message = "*Good Morning Everyone!*\n\n" \
-                  "*Current Status of COVID-19 in India as on _" + date + "_*\n\n" \
-                  "```Total Number of Active Covid-19 Cases so far across India : " + str(active) + "\n\n" \
-                  "Total Number of Cases(Indian National)    : " + str(ind_national) + "\n" \
-                  "Total Number of Cases(Foreign National)   : " + str(foreign_national) + "\n" \
-                  "Total Number of Cured/Migrated Cases      : " + str(cured) + "\n" \
-                  "Total Number of Deaths due to Covid-19    : " + str(deaths) + "\n\n" \
-                  "Total Count of Confirmed Covid-19 Cases so far across India : " + str(total) + "```\n" \
-                  "*For State Wise Report Visit -> <https://www.mohfw.gov.in/|Ministry of Health & Family Welfare>*\n\n" \
-                  "*Follow Hygiene! Stay Home! And Stay Safe!*"
+        url = webhook_url
 
         bot_message = {
                     "text": message
@@ -60,49 +44,52 @@ class CovidIndia():
                 row = [i.text for i in td]
                 table_data.append(row)
 
-            report = ['Consolidated Report'] + table_data[len(table_data) - 1]
+            KA_Report = None
+            IN_Report_1 = None
+            for table in table_data:
+                for data in table:
+                    if data == "Karnataka":
+                        KA_Report = table
+                    if data == "Total number of confirmed cases in India":
+                        IN_Report_1 = table
 
-            print(report)
-            cons_report = []
-            for element in report:
-                cons_report.append(re.sub('[^0-9,.]', '', element))
+            print(KA_Report)
+            print(IN_Report_1)
 
-            print(cons_report)
+            fail_report = "*Report generation is failed!*"
+            sap_automation = "https://chat.googleapis.com/v1/spaces/AAAAimyyXHo/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=u-1wNxlL_j45X0AK9iLHlBMok4ZLF779YpGv59mLXc8%3D"
+            SS_India_Group = "https://chat.googleapis.com/v1/spaces/AAAAimyyXHo/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=u-1wNxlL_j45X0AK9iLHlBMok4ZLF779YpGv59mLXc8%3D"
 
-            active_cases = int(cons_report[2]) + int(cons_report[3]) - int(cons_report[4]) - int(cons_report[5])
-            total_cases = int(cons_report[2]) + int(cons_report[3]) + int(cons_report[4]) + int(cons_report[5])
+            if IN_Report_1 is None:
+                self.smartshift_Bot(fail_report, sap_automation)
+                sys.exit()
 
-            print("Total number of Active COVID 2019 cases across India :", active_cases)
-            print("Total number of Cured/Discharged COVID 2019 cases across India :", cons_report[4])
-            print("Total number of Deaths due to COVID 2019 across India :", cons_report[5])
-            print("Total count of cases :", total_cases)
+            IN_Report = []
+            for element in IN_Report_1:
+                IN_Report.append(re.sub('[^0-9,.]', '', element))
 
-            #data = table_data[6:-1]
+            print(IN_Report)
 
-            #table = PrettyTable()
-            #table.title = 'Covid-19 report of India'
-            #table.field_names = ['SL.No', 'State', 'Confirmed Cases[Indian National]',
-             #                    'Confirmed Cases[Foreign National]', 'Cured/Discharged/Migrated', 'Death']
+            active_cases = int(IN_Report[1]) - int(IN_Report[2]) - int(IN_Report[3])
+            total_cases = int(IN_Report[1])
+            active_KA = int(KA_Report[2])
 
-            #for row in data:
-              #  table.add_row(row)
+            currentDT = datetime.datetime.now()
 
-            #table.add_row(['--------------', '--------------', '--------------', '--------------', '--------------',
-             #              '--------------'])
-            #table.add_row(cons_report)
+            date = currentDT.strftime("%a, %b %d, %Y") + ' at ' + currentDT.strftime("%I:%M:%S %p")
 
-            #print(data)
+            report = "*Good Morning Everyone!*\n\n" \
+                     "*Current Status of COVID-19 in India as on _" + date + "_*\n\n" \
+                     "```Total Number of Active Covid-19 Cases so far across India : " + str(active_cases) + "\n\n" \
+                     "Total Number of Cured/Migrated Cases      : " + str(IN_Report[2]) + "\n" \
+                     "Total Number of Deaths due to Covid-19    : " + str(IN_Report[3]) + "\n" \
+                     "Total Number of Active cases in Karnataka : " + str(active_KA) + "\n\n" \
+                     "Total Confirmed Covid-19 Cases registered so far across India : " + str(total_cases) + "```\n" \
+                     "*For State Wise Report Visit -> <https://www.mohfw.gov.in/|Ministry of Health & Family Welfare>*\n\n" \
+                     "*Follow Hygiene! Stay Home! And Stay Safe!*"
 
-            #df = pd.DataFrame(data, columns=['SL.No', 'State', 'Indian National', 'Foreign National', 'Cured', 'Death'])
-            #print(df)
-            #print(table)
+            self.smartshift_Bot(report, sap_automation)
 
-            #f = open("data.txt", "w")
-            #f.write(str(df))
-            #f.close()
 
-            self.hangout(active_cases, cons_report[2], cons_report[3], cons_report[4], cons_report[5], total_cases)
-
-s = CovidIndia()
+s = Covid19_Report_IN()
 print(s.covid_report())
-#s.hangout()
